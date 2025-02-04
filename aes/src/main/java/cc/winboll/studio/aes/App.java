7f8ee8d6de623f34c2bbcd310b043cd89946cab1
ZhanGSKen <ZhanGSKen@AliYun.Com>
2025-02-04 09:00:34 +0800

    更新类库

--- aes/src/main/java/cc/winboll/studio/aes/App.java
+++ aes/src/main/java/cc/winboll/studio/aes/App.java
@@ -5,9 +5,10 @@ package cc.winboll.studio.aes;
  * @Date 2024/06/13 19:03:58
  * @Describe AES应用类
  */
-import cc.winboll.studio.GlobalApplication;
+import cc.winboll.studio.libappbase.GlobalApplication;
+import cc.winboll.studio.libapputils.app.WinBollGlobalApplication;
 
-public class App extends GlobalApplication {
+public class App extends WinBollGlobalApplication {
     
     public static final String TAG = "App";
 
