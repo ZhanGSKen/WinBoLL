75b2245061ca0a1bb0000a0fa33d1a8039d3100b
ZhanGSKen <ZhanGSKen@AliYun.Com>
2025-02-12 23:28:42 +0800

    添加初始 Contacts 项目

---
+++ contacts/README.md
@@ -0,0 +1,38 @@
+# Contacts
+
+#### 介绍
+通讯录与拨号
+
+#### 软件架构
+适配安卓应用 [AIDE Pro] 的 Gradle 编译结构。
+也适配安卓应用 [AndroidIDE] 的 Gradle 编译结构。
+
+
+#### Gradle 编译说明
+调试版编译命令 ：gradle assembleBetaDebug
+阶段版编译命令 ：gradle assembleStageRelease
+
+#### 使用说明
+
+在安卓系统中需要设置两个权限允许。
+1.自启动权限允许。
+2.省电策略-无限制权限允许。
+
+#### 参与贡献
+
+1.  Fork 本仓库
+2.  新建 Feat_xxx 分支
+3.  提交代码 : ZhanGSKen(ZhanGSKen@AliYun.Com)
+4.  新建 Pull Request
+
+
+#### 特技
+
+1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
+2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
+3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
+4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
+5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
+6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+
+#### 参考文档
---
+++ contacts/src/main/java/cc/winboll/studio/contacts/MainActivity.java
@@ -0,0 +1,205 @@
+package cc.winboll.studio.contacts;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import cc.winboll.studio.contacts.BuildConfig;
+import cc.winboll.studio.contacts.R;
+import cc.winboll.studio.libappbase.LogUtils;
+import cc.winboll.studio.libapputils.app.IWinBollActivity;
+import cc.winboll.studio.libapputils.app.WinBollActivityManager;
+import cc.winboll.studio.libapputils.bean.APPInfo;
+import cc.winboll.studio.libapputils.util.UriUtils;
+import cc.winboll.studio.libapputils.view.StringToQrCodeView;
+import cc.winboll.studio.libapputils.view.YesNoAlertDialog;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+import cc.winboll.studio.libapputils.app.AboutActivityFactory;
+
+final public class MainActivity extends AppCompatActivity implements IWinBollActivity {
+
+	public static final String TAG = "MainActivity";
+
+    public static final int REQUEST_HOME_ACTIVITY = 0;
+    public static final int REQUEST_ABOUT_ACTIVITY = 1;
+
+    Toolbar mToolbar;
+    
+    @Override
+    public AppCompatActivity getActivity() {
+        return this;
+    }
+    
+    @Override
+    public APPInfo getAppInfo() {
+        String szBranchName = "contacts";
+
+        APPInfo appInfo = AboutActivityFactory.buildDefaultAPPInfo();
+        appInfo.setAppName("Contacts");
+        appInfo.setAppIcon(cc.winboll.studio.libapputils.R.drawable.ic_winboll);
+        appInfo.setAppDescription("Contacts Description");
+        appInfo.setAppGitName("APP");
+        appInfo.setAppGitOwner("Studio");
+        appInfo.setAppGitAPPBranch(szBranchName);
+        appInfo.setAppGitAPPSubProjectFolder(szBranchName);
+        appInfo.setAppHomePage("https://www.winboll.cc/studio/details.php?app=Contacts");
+        appInfo.setAppAPKName("Contacts");
+        appInfo.setAppAPKFolderName("Contacts");
+        return appInfo;
+        //return null;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // 接收并处理 Intent 数据，函数 Intent 处理接收就直接返回
+        //if (prosessIntents(getIntent())) return;
+        // 以下正常创建主窗口
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 初始化工具栏
+        mToolbar = findViewById(R.id.activitymainToolbar1);
+        setSupportActionBar(mToolbar);
+        if (isEnableDisplayHomeAsUp()) {
+            // 显示后退按钮
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        getSupportActionBar().setSubtitle(getTag());
+        
+        //ToastUtils.show("WinBollUI_TYPE " + WinBollApplication.getWinBollUI_TYPE());
+        LogUtils.d(TAG, "BuildConfig.DEBUG : " + Boolean.toString(BuildConfig.DEBUG));
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        //setSubTitle("");
+    }
+
+    //
+    // 处理传入的 Intent 数据
+    //
+//    boolean prosessIntents(Intent intent) {
+//        if (intent == null 
+//            || intent.getAction() == null
+//            || intent.getAction().equals(""))
+//            return false;
+//
+//        if (intent.getAction().equals(StringToQrCodeView.ACTION_UNITTEST_QRCODE)) {
+//            try {
+//                WinBollActivity clazzActivity = UnitTestActivity.class.newInstance();
+//                String tag = clazzActivity.getTag();
+//                LogUtils.d(TAG, "String tag = clazzActivity.getTag(); tag " + tag);
+//                Intent subIntent = new Intent(this, UnitTestActivity.class);
+//                subIntent.setAction(intent.getAction());
+//                File file = new File(getCacheDir(), UUID.randomUUID().toString());
+//                //取出文件uri
+//                Uri uri = intent.getData();
+//                if (uri == null) {
+//                    uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+//                }
+//                //获取文件真实地址
+//                String szSrcPath = UriUtils.getFileFromUri(getApplication(), uri);
+//                if (TextUtils.isEmpty(szSrcPath)) {
+//                    return false;
+//                }
+//
+//                Files.copy(Paths.get(szSrcPath), Paths.get(file.getPath()));
+//                //startWinBollActivity(subIntent, tag);
+//                WinBollActivityManager.getInstance(this).startWinBollActivity(this, subIntent, UnitTestActivity.class);
+//            } catch (IllegalAccessException | InstantiationException | IOException e) {
+//                LogUtils.d(TAG, e, Thread.currentThread().getStackTrace());
+//                // 函数处理异常返回失败
+//                return false;
+//            }
+//        } else {
+//            LogUtils.d(TAG, "prosessIntents|" + intent.getAction() + "|yet");
+//            return false;
+//        }
+//        return true;
+//    }
+
+    @Override
+    public String getTag() {
+        return TAG;
+    }
+
+    @Override
+    public Toolbar initToolBar() {
+        return findViewById(R.id.activitymainToolbar1);
+    }
+
+    @Override
+    public boolean isAddWinBollToolBar() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnableDisplayHomeAsUp() {
+        return false;
+    }
+    
+    @Override
+    public void onBackPressed() {
+        exit();
+    }
+
+    void exit() {
+        YesNoAlertDialog.OnDialogResultListener listener = new YesNoAlertDialog.OnDialogResultListener(){
+
+            @Override
+            public void onYes() {
+                WinBollActivityManager.getInstance(getApplicationContext()).finishAll();
+            }
+
+            @Override
+            public void onNo() {
+            }
+        };
+        YesNoAlertDialog.show(this, "[ " + getString(R.string.app_name) + " ]", "Exit(Yes/No).\nIs close all activity?", listener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == R.id.item_unittest) {
+//            WinBollActivityManager.getInstance(this).startWinBollActivity(this, UnitTestActivity.class);
+//        } else 
+//        if (item.getItemId() == R.id.item_exit) {
+//            exit();
+//            return true;
+//        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (resultCode) {
+            case REQUEST_HOME_ACTIVITY : {
+                    LogUtils.d(TAG, "REQUEST_HOME_ACTIVITY");
+                    break;
+                }
+            case REQUEST_ABOUT_ACTIVITY : {
+                    LogUtils.d(TAG, "REQUEST_ABOUT_ACTIVITY");
+                    break;
+                }
+            default : {
+                    super.onActivityResult(requestCode, resultCode, data);
+                }
+        }
+    }
+}
