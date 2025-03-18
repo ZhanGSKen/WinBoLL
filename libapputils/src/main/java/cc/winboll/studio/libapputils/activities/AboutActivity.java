dc1b138dd7e9523f4406f9d2da4bc03f3418a8d6
ZhanGSKen <ZhanGSKen@AliYun.Com>
2025-03-19 01:30:40 +0800

    20250319_0130

--- libapputils/src/main/java/cc/winboll/studio/libapputils/activities/AboutActivity.java
+++ libapputils/src/main/java/cc/winboll/studio/libapputils/activities/AboutActivity.java
@@ -5,14 +5,15 @@ package cc.winboll.studio.libapputils.activities;
  * @Date 2024/07/14 13:20:33
  * @Describe 应用介绍页
  */
-import cc.winboll.studio.libapputils.R;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.Toolbar;
+import cc.winboll.studio.libapputils.R;
 import cc.winboll.studio.libapputils.app.IWinBollActivity;
 import cc.winboll.studio.libapputils.app.WinBollActivityManager;
 import cc.winboll.studio.libapputils.bean.APPInfo;
@@ -28,7 +29,7 @@ final public class AboutActivity extends Activity implements IWinBollActivity {
     APPInfo mAPPInfo;
 
     @Override
-    public Activity getActivity() {
+    public Context getApplicationContext() {
         return this;
     }
 
--- libapputils/src/main/java/cc/winboll/studio/libapputils/app/IWinBollActivity.java
+++ libapputils/src/main/java/cc/winboll/studio/libapputils/app/IWinBollActivity.java
@@ -5,7 +5,7 @@ package cc.winboll.studio.libapputils.app;
  * @Date 2025/03/08 00:15:36
  * @Describe WinBoll 活动窗口通用接口
  */
-import android.app.Activity;
+import android.content.Context;
 import android.widget.Toolbar;
 import cc.winboll.studio.libapputils.bean.APPInfo;
 
@@ -13,8 +13,8 @@ public interface IWinBollActivity {
 
     public static final String TAG = "IWinBollActivity";
 
-    // 获取当前具有 IWinBoll 特征的 AppCompatActivity 活动窗口
-    abstract public Activity getActivity();
+    // 获取应用资源上下文
+    abstract public Context getApplicationContext();
     abstract public APPInfo getAppInfo();
     abstract public String getTag();
     abstract public Toolbar initToolBar();
