3b61a934024fe0988bca4b7a2b905453e1bcc67a
ZhanGSKen <ZhanGSKen@AliYun.Com>
2025-03-25 03:28:39 +0800

    移动应用基础数据模型到 APPBase 类库。

--- apputils/src/main/java/cc/winboll/studio/apputils/App.java
+++ apputils/src/main/java/cc/winboll/studio/apputils/App.java
@@ -7,10 +7,7 @@ package cc.winboll.studio.apputils;
  */
 import android.app.Application;
 import android.content.Context;
-import android.widget.Toast;
 import cc.winboll.studio.libappbase.GlobalApplication;
-import cc.winboll.studio.libappbase.LogUtils;
-import cc.winboll.studio.libapputils.bean.DebugBean;
 
 public class App extends GlobalApplication {
 
@@ -49,37 +46,5 @@ public class App extends GlobalApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        // 应用环境初始化, 基本调试环境
-        //
-        // 初始化日志模块
-        //LogUtils.init(this);
-
-        try {
-            // 初始化 Toast 框架
-//            ToastUtils.init(this);
-//            // 设置 Toast 布局样式
-//            //ToastUtils.setView(R.layout.view_toast);
-//            ToastUtils.setStyle(new WhiteToastStyle());
-//            ToastUtils.setGravity(Gravity.BOTTOM, 0, 200);
-            // 设置应用调试标志
-            DebugBean debugBean = DebugBean.loadBean(this, DebugBean.class);
-            if (debugBean == null) {
-                //ToastUtils.show("debugBean == null");
-                setIsDebug(false);
-            } else {
-                //ToastUtils.show("saveDebugStatus(" + String.valueOf(debugBean.isDebuging()) + ")");
-                setIsDebug(debugBean.isDebuging());
-            }
-            // 应用窗口管理模块参数设置
-            //
-            //mMyActivityLifecycleCallbacks = new MyActivityLifecycleCallbacks();
-            //registerActivityLifecycleCallbacks(mMyActivityLifecycleCallbacks);
-            // 设置默认 WinBoll 应用 UI 类型
-            //WinBollActivityManager.getInstance(this).setWinBollUI_TYPE(WinBollActivityManager.WinBollUI_TYPE.Service);
-            //ToastUtils.show("WinBollUI_TYPE " + getWinBollUI_TYPE());
-        } catch (Exception e) {
-            LogUtils.d(TAG, e, Thread.currentThread().getStackTrace());
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
     }
 }
