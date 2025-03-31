99ff741a224a80e1835069f1a7289bafaecb3daf
ZhanGSKen <ZhanGSKen@AliYun.Com>
2025-03-31 14:53:33 +0800

    更新类库，设置版本号。

--- powerbell/src/main/java/cc/winboll/studio/powerbell/App.java
+++ powerbell/src/main/java/cc/winboll/studio/powerbell/App.java
@@ -1,12 +1,12 @@
 package cc.winboll.studio.powerbell;
 
 import android.content.Context;
+import android.view.Gravity;
 import cc.winboll.studio.libappbase.GlobalApplication;
-import cc.winboll.studio.libappbase.utils.ToastUtils;
 import cc.winboll.studio.powerbell.receivers.GlobalApplicationReceiver;
 import cc.winboll.studio.powerbell.utils.AppCacheUtils;
 import cc.winboll.studio.powerbell.utils.AppConfigUtils;
-import cc.winboll.studio.libapputils.bean.DebugBean;
+import com.hjq.toast.ToastUtils;
 
 public class App extends GlobalApplication {
 
@@ -20,22 +20,13 @@ public class App extends GlobalApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        // 设置应用调试标志
-        DebugBean debugBean = DebugBean.loadBean(this, DebugBean.class);
-        if (debugBean == null) {
-            //ToastUtils.show("debugBean == null");
-            setIsDebuging(this, false);
-        } else {
-            //ToastUtils.show("saveDebugStatus(" + String.valueOf(debugBean.isDebuging()) + ")");
-            setIsDebuging(this, debugBean.isDebuging());
-        }
         
         // 初始化 Toast 框架
-        //ToastUtils.init(this);
+        ToastUtils.init(this);
         // 设置 Toast 布局样式
         //ToastUtils.setView(R.layout.toast_custom_view);
         //ToastUtils.setStyle(new WhiteToastStyle());
-        //ToastUtils.setGravity(Gravity.BOTTOM, 0, 200);
+        ToastUtils.setGravity(Gravity.BOTTOM, 0, 200);
 
         // 设置数据配置存储工具
         _mAppConfigUtils = getAppConfigUtils(this);
