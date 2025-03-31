f572c488e617014ce739864301bec916cce07b48
ZhanGSKen <ZhanGSKen@AliYun.Com>
2025-03-31 15:42:33 +0800

    修改调试模式配置方法

--- contacts/src/main/java/cc/winboll/studio/contacts/App.java
+++ contacts/src/main/java/cc/winboll/studio/contacts/App.java
@@ -5,8 +5,8 @@ package cc.winboll.studio.contacts;
  * @Date 2024/12/08 15:10:51
  * @Describe 全局应用类
  */
+import android.view.Gravity;
 import cc.winboll.studio.libappbase.GlobalApplication;
-import cc.winboll.studio.libappbase.LogUtils;
 import cc.winboll.studio.libappbase.winboll.WinBollActivityManager;
 import com.hjq.toast.ToastUtils;
 
@@ -18,14 +18,20 @@ public class App extends GlobalApplication {
     public void onCreate() {
         // 必须在调用基类前设置应用调试标志，
         // 这样可以预先设置日志与数据的存储根目录。
-        setIsDebuging(BuildConfig.DEBUG);
+        //setIsDebuging(BuildConfig.DEBUG);
         super.onCreate();
         // 设置 WinBoll 应用 UI 类型
         WinBollActivityManager.getInstance(this).setWinBollUI_TYPE(WinBollActivityManager.WinBollUI_TYPE.Aplication);
         
-        LogUtils.d(TAG, "onCreate");
+        //LogUtils.d(TAG, "onCreate");
         
+        // 初始化 Toast 框架
         ToastUtils.init(this);
+        // 设置 Toast 布局样式
+        //ToastUtils.setView(R.layout.toast_custom_view);
+        //ToastUtils.setStyle(new WhiteToastStyle());
+        ToastUtils.setGravity(Gravity.BOTTOM, 0, 200);
+        
     }
 
 }
