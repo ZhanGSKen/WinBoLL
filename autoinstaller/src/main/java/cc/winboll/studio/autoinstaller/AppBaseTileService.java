7edc341f1562ba4baffd28653386644e15a1995f
ZhanGSKen <ZhanGSKen@AliYun.Com>
2025-04-15 16:03:45 +0800

    整理代码

--- autoinstaller/src/main/java/cc/winboll/studio/autoinstaller/AppBaseTileService.java
+++ autoinstaller/src/main/java/cc/winboll/studio/autoinstaller/AppBaseTileService.java
@@ -1,17 +1,18 @@
 package cc.winboll.studio.autoinstaller;
-import android.content.Context;
-import android.service.quicksettings.Tile;
-import android.service.quicksettings.TileService;
-import cc.winboll.studio.autoinstaller.models.MainServiceBean;
-import cc.winboll.studio.autoinstaller.services.MainService;
-import cc.winboll.studio.autoinstaller.models.AppConfigs;
 
 /**
  * @Author ZhanGSKen@AliYun.Com
  * @Date 2025/04/15 09:24:46
  * @Describe 磁贴工具服务类
  */
+import android.content.Context;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import cc.winboll.studio.autoinstaller.models.AppConfigs;
+import cc.winboll.studio.autoinstaller.models.MainServiceBean;
+
 public class AppBaseTileService extends TileService {
+    
     public static final String TAG = "AppBaseTileService";
 
     volatile static AppBaseTileService _AppBaseTileService;
@@ -21,23 +22,15 @@ public class AppBaseTileService extends TileService {
         super.onStartListening();
         _AppBaseTileService = this;
         Tile tile = getQsTile();
-        //MainServiceBean bean = MainServiceBean.loadBean(this, MainServiceBean.class);
-        
+
         if (AppConfigs.getInstance(AppBaseTileService.this).isEnableService()) {
-            //MainService.startMainService(context);
             tile.setState(Tile.STATE_ACTIVE);
             tile.setIcon(android.graphics.drawable.Icon.createWithResource(this, R.drawable.ic_cloud));
         } else {
-            //MainService.stopMainService(context);
             tile.setState(Tile.STATE_INACTIVE);
             tile.setIcon(android.graphics.drawable.Icon.createWithResource(this, R.drawable.ic_cloud_outline));
         }
         tile.updateTile();
-//        Tile tile = getQsTile();
-//        tile.setState(Tile.STATE_INACTIVE);
-//        tile.setLabel(getString(R.string.tileservice_name));
-//        tile.setIcon(android.graphics.drawable.Icon.createWithResource(this, R.drawable.ic_cloud_outline));
-//        tile.updateTile();
 
     }
 
@@ -45,14 +38,8 @@ public class AppBaseTileService extends TileService {
     public void onClick() {
         super.onClick();
         Tile tile = getQsTile();
-//        MainServiceBean bean = MainServiceBean.loadBean(this, MainServiceBean.class);
-//        if (bean == null) {
-//            bean = new MainServiceBean();
-//        }
 
         if (tile.getState() == Tile.STATE_ACTIVE) {
-//            bean.setIsEnable(false);
-//            MainServiceBean.saveBean(this, bean);
             AppConfigs.getInstance(AppBaseTileService.this).setIsEnableService(false);
             AppConfigs.getInstance(AppBaseTileService.this).saveAppConfigs();
             MainActivity.stopMainService();
