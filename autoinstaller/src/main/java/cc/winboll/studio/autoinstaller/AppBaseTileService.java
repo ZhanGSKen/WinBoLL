dabd1271fded34c572e87d7c50d7f1c61430ecf1
ZhanGSKen <ZhanGSKen@AliYun.Com>
2025-04-15 17:23:19 +0800

    重构磁贴状态更新方法

--- autoinstaller/src/main/java/cc/winboll/studio/autoinstaller/AppBaseTileService.java
+++ autoinstaller/src/main/java/cc/winboll/studio/autoinstaller/AppBaseTileService.java
@@ -17,53 +17,27 @@ public class AppBaseTileService extends TileService {
     public void onStartListening() {
         super.onStartListening();
         Tile tile = getQsTile();
-
-        if (AppConfigs.getInstance(AppBaseTileService.this).isEnableService()) {
-            tile.setState(Tile.STATE_ACTIVE);
-            tile.setIcon(android.graphics.drawable.Icon.createWithResource(this, R.drawable.ic_android));
-        } else {
-            tile.setState(Tile.STATE_INACTIVE);
-            tile.setIcon(android.graphics.drawable.Icon.createWithResource(this, R.drawable.ic_android));
-        }
+        tile.setIcon(android.graphics.drawable.Icon.createWithResource(this, R.drawable.ic_android));
+        // 更新磁贴状态
+        tile.setState(AppConfigs.getInstance(getApplicationContext()).isEnableService()?Tile.STATE_ACTIVE: Tile.STATE_INACTIVE);
         tile.updateTile();
     }
 
     @Override
     public void onClick() {
         super.onClick();
-        Tile tile = getQsTile();
-
-        if (tile.getState() == Tile.STATE_ACTIVE) {
-            AppConfigs.getInstance(AppBaseTileService.this).setIsEnableService(false);
-            AppConfigs.getInstance(AppBaseTileService.this).saveAppConfigs();
-            MainActivity.stopMainService();
-        } else if (tile.getState() == Tile.STATE_INACTIVE) {
-            AppConfigs.getInstance(AppBaseTileService.this).setIsEnableService(true);
-            AppConfigs.getInstance(AppBaseTileService.this).saveAppConfigs();
-            MainActivity.startMainService();
-        }
-
-        // 更新磁贴状态
-        //updateServiceIconStatus(this);
-    }
-
-    //
-    // 更新磁贴状态
-    //
-//    public static void updateServiceIconStatus(Context context) {
-//        if (_AppBaseTileService == null) {
-//            return;
-//        }
+//        Tile tile = getQsTile();
 //
-//        Tile tile = _AppBaseTileService.getQsTile();
-//        MainServiceBean bean = MainServiceBean.loadBean(context, MainServiceBean.class);
-//        if (bean != null && bean.isEnable()) {
-//            tile.setState(Tile.STATE_ACTIVE);
-//            tile.setIcon(android.graphics.drawable.Icon.createWithResource(context, R.drawable.ic_cloud));
-//        } else {
+//        if (tile.getState() == Tile.STATE_ACTIVE) {
 //            tile.setState(Tile.STATE_INACTIVE);
-//            tile.setIcon(android.graphics.drawable.Icon.createWithResource(context, R.drawable.ic_cloud_outline));
+//            AppConfigs.getInstance(AppBaseTileService.this).setIsEnableService(false);
+//            AppConfigs.getInstance(AppBaseTileService.this).saveAppConfigs();
+//            MainActivity.stopMainService();
+//        } else if (tile.getState() == Tile.STATE_INACTIVE) {
+//            tile.setState(Tile.STATE_ACTIVE);
+//            AppConfigs.getInstance(AppBaseTileService.this).setIsEnableService(true);
+//            AppConfigs.getInstance(AppBaseTileService.this).saveAppConfigs();
+//            MainActivity.startMainService();
 //        }
-//        tile.updateTile();
-//    }
+    }
 }
