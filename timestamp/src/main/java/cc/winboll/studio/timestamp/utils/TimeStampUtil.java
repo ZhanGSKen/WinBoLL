29785be88df38eebf5408844cc246241dec4101f
ZhanGSKen <zhangsken@188.com>
2025-05-07 13:46:21 +0800

    修复信息显示

--- timestamp/src/main/java/cc/winboll/studio/timestamp/utils/TimeStampUtil.java
+++ timestamp/src/main/java/cc/winboll/studio/timestamp/utils/TimeStampUtil.java
@@ -39,7 +39,7 @@ public class TimeStampUtil {
         long currentMillis = mTimeStamp;
         Instant instant = Instant.ofEpochMilli(currentMillis);
         LocalDateTime ldt = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-        String szTimeStampFormatString = AppConfigsUtil.getInstance(mContext).getAppConfigsModel().getTimeStampCopyFormatString();
+        String szTimeStampFormatString = AppConfigsUtil.getInstance(mContext).getAppConfigsModel().getTimeStampFormatString();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(szTimeStampFormatString);
         String formattedDateTime = ldt.format(formatter);
         return formattedDateTime;
