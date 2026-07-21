eebb21d0418eaac8b7495697d938a8d84b6c818a
ZhanGSKen <ZhanGSKen@QQ.COM>
2026-07-21 12:48:06 +0800

    添加应用异常信息邮件接收账号设置窗口资源文件。

---
+++ appbase/src/main/java/cc/winboll/studio/appbase/develop/APPExcetionMailSettingsActivity.java
@@ -0,0 +1,23 @@
+package cc.winboll.studio.appbase.develop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cc.winboll.studio.appbase.R;
+
+/**
+ * @Author 豆包&BigPickle&MiMo&ZhanGSKen<zhangsken@qq.com>
+ * @Date 2026/07/21 12:44
+ * @Describe 应用异常信息邮件接收账号设置
+ */
+public class APPExcetionMailSettingsActivity extends Activity {
+    
+    public static final String TAG = "APPExcetionMailSettingsActivity";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appexcetionmailsettings);
+        
+    }
+    
+}
