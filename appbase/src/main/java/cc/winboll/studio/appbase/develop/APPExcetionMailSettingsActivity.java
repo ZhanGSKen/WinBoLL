9a529cf030a1b9a0f81bc1de40c617e57b2fed81
BigPickle <BigPickle@WinBoLL.CC>
2026-07-21 18:06:04 +0800

    refactor(appbase): SMTPConfigView改用libaes内置版本并修复R8混淆崩溃
    - APPExcetionMailSettingsActivity引用改为cc.winboll.studio.libaes.views.SMTPConfigView
    - 删除libappbase自定义SMTPConfigView
    - appbase添加JavaMail依赖(android-mail/android-activation 1.6.7)
    - proguard-rules保留javax.mail.**修复NoClassDefFoundError
    - packagingOptions排除重复META-INF文件

--- appbase/src/main/java/cc/winboll/studio/appbase/develop/APPExcetionMailSettingsActivity.java
+++ appbase/src/main/java/cc/winboll/studio/appbase/develop/APPExcetionMailSettingsActivity.java
@@ -3,13 +3,13 @@ package cc.winboll.studio.appbase.develop;
 import android.app.Activity;
 import android.os.Bundle;
 import cc.winboll.studio.appbase.R;
-import cc.winboll.studio.libappbase.widget.SMTPConfigView;
+import cc.winboll.studio.libaes.views.SMTPConfigView;
 
 /**
  * @Author 豆包&BigPickle&MiMo&ZhanGSKen<zhangsken@qq.com>
  * @Date 2026/07/21 12:44
  * @Describe 应用异常信息邮件接收账号设置
- * 使用SMTPConfigView控件配置SMTP服务器参数
+ * 使用libaes的SMTPConfigView控件配置SMTP服务器参数
  */
 public class APPExcetionMailSettingsActivity extends Activity {
 
